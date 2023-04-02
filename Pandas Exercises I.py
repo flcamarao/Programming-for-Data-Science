c94edec8b7873f3a11a1c1f9ce25ba7af4dc99b7
Francis Camarao <99628537+flcamarao@users.noreply.github.com>
2023-04-03 02:34:33 +0800

    Update Pandas Exercises I.py

--- Pandas Exercises I.py
+++ Pandas Exercises I.py
@@ -10,45 +10,38 @@ def init_df(s1, s2):
     return df1, df2
 
 def top_plus_bottom():
-    filepath = '/mnt/data/public/retaildata/Online Retail.csv'
     df = pd.read_csv(filepath, usecols = ['Quantity', 'UnitPrice'])
     return df.head(5).to_numpy() + df.tail(5).to_numpy()
 
 def count_alone():
-    filepath = '/mnt/data/public/elections/comelec/voters_profile/philippine_2016_voter_profile_by_age_group.csv'
     df = pd.read_csv(filepath)
     df['alone'] = df['single'] + df['widow'] + df['legally_seperated']
     df.sort_values(by='alone', ascending=False)
     return df
 
 def scores_stats():
-    filepath = '/mnt/data/public/movielens/20m/ml-20m/genome-scores.csv'
     df = pd.read_csv(filepath)
     df = df['relevance'].describe()
     return df
 
 def top_cat():
-    filepath = '/mnt/data/public/agora/Agora.csv'
     df = pd.read_csv(filepath, encoding='latin1', skipinitialspace=True)
     categories = df['Category'].value_counts().head(10)
     return categories
 
 def listing_info():
-    filepath = '/mnt/data/public/insideairbnb/data.insideairbnb.com/united-kingdom/england/london/2015-04-06/data/listings.csv.gz'
     df = pd.read_csv(filepath, usecols=['id','name', 'summary' , 'space' , 'description'], index_col='id')
     df = df.sort_index()
     df = df.query('id >= 11076 and id <= 15400')
     return df
 
 def aisle_dep():
-    filepath = '/mnt/data/public/instacart/instacart_2017_05_01/products.csv'
     df = pd.read_csv(filepath, index_col='product_id')
     df.loc[df['aisle_id'] == 5, 'product_name'] = (df['product_name'] + " (" + 
     df['aisle_id'].astype(str) + "-" + df['department_id'].astype(str)) + ")"
     return df
 
 def camsur_reps():
-    filepath = '/mnt/data/public/elections/comelec/congress_results/congressional_results_2013.csv'
     df = pd.read_csv(filepath)
     df = df[(df['province_or_city'] == 'Camarines Sur')]
     df['surname'] = df['name'].str.split(',', expand=True)[0]
@@ -56,7 +49,6 @@ def camsur_reps():
     return df
 
 def no_pop():
-    filepath = '/mnt/data/public/millionsong/AdditionalFiles/tracks_per_year.txt'
     df = pd.read_csv(filepath, sep='<SEP>', engine ='python', header=None )
     df.columns = ['year', 'track_id', 'artist', 'title']
     options = ['Britney Spears' ,'Backstreet Boys'] 
@@ -64,7 +56,6 @@ def no_pop():
     return df 
 
 def read_trips():
-    filepath = '/mnt/data/public/nyctaxi/trip_data/trip_data_1.csv'
     df = pd.read_csv(filepath, nrows=100)
     df['rate_code'] = df['rate_code'].astype(str)
     df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'])
